f8369bb448b5a49aa3748c2adff434469425a313
3q12 <si8363@naver.com>
2016-11-16 15:33:47 +0900

    simple struct

---
+++ my_fs.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>a
+
+union type 
+{
+	 struct block *indirectnodei[16];
+	 char body[128];
+}
+
+struct block{
+	 union type;
+}
+struct Dir{
+	 
+}
+struct Inode{
+	 _Bool ForD;
+	// time
+	 int File_size;
+	 struct block direct;
+	 struct block indirect;
+	 struct block double_indirect
+}
