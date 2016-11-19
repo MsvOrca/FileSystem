458655908f080e11233345ea29496fac1492a50a
MsvOrca <wjp229@naver.com>
2016-11-19 20:48:32 +0900

    시간 2차

--- time.c
+++ time.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-typedef struct{//시간에 대한 구조체
-	int year;
-	int mon;
-	int day;
-	int hour;
-	int min;
-	int sec;
-}Touched;
 typedef struct{
-	Touched *made;//아이노드에 넣을 구조체 예시
+	char current[20];//아이노드에 넣을 구조체 예시
 }Inode;
-void INPUT_TIME(Inode test){
+void INPUT_TIME(char *current){
 	struct tm *t;
 	time_t timer;
+	int len;
 	timer = time(NULL);
 	t = localtime(&timer);
 
-	test.made->year = t -> tm_year + 1900;
-	test.made->mon = t -> tm_mon + 1;
-	test.made->day = t -> tm_mday;
-	test.made->hour = t -> tm_hour;
-	test.made->min = t -> tm_min;
-	test.made->sec = t -> tm_sec;
-
-	return;
+	len = sprintf(current, "%d",t -> tm_year + 1900);
+	*(current + len) = '/';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_mon + 1);
+	*(current + len) = '/';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_mday);
+	*(current + len) = ' ';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_hour);
+	*(current + len) = ':';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_min);
+	*(current + len) = ':';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_sec);
 }
 int main()
 {
 	Inode test;
-	test.made = malloc(sizeof(Touched));
 
-	INPUT_TIME(test);
+	INPUT_TIME(test.current);
 
-	printf("%d/%d/%d %d:%d:%d", test.made->year, test.made->mon, test.made->day, test.made->hour, test.made->min, test.made->sec);
+	printf("%s", test.current);
 	return 0;
 }
