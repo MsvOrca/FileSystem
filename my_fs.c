25cec6364fdf2e649febf3f3b67f28c88671ab09
3q12 <si8363@naver.com>
2016-11-21 13:16:29 +0900

    add

--- my_fs.c
+++ my_fs.c
@@ -2,25 +2,17 @@
 #include<time.h>
 
 void INPUT_TIME(char *current);
-
-typedef struct Block;
-
-union type 
-{
-	 Block *indirectnode[16];
-	 char file[128];
-};
-
 typedef struct Block{
-	 union type;
+	 struct Block *indirectnode[16];
+	 char file[128];
 }Block;
-typedef struct File{
+typedef struct{
 
 }File;
 typedef struct Dir{
-	Dir *pPrevDir; //상위 디렉토리
-	Dir *pSimilDir;//동위 디렉토리
-	Dir *pNextDir;//하위 디렉토리
+	struct Dir *pPrevDir; //상위 디렉토리
+	struct Dir *pSimilDir;//동위 디렉토리
+	struct Dir *pNextDir;//하위 디렉토리
 
 	File *pFileData;	 
 }Dir;
@@ -38,26 +30,26 @@ int main()
 }
 
 void INPUT_TIME(char *current){
-	struct tm *t;
-	time_t timer;
-	int len;
-	timer = time(NULL);
-	t = localtime(&timer);
+	 struct tm *t;
+	 time_t timer;
+	 int len;
+	 timer = time(NULL);
+	 t = localtime(&timer);
 
-	len = sprintf(current, "%d",t -> tm_year + 1900);
-	*(current + len) = '/';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_mon + 1);
-	*(current + len) = '/';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_mday);
-	*(current + len) = ' ';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_hour);
-	*(current + len) = ':';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_min);
-	*(current + len) = ':';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_sec);
+	 len = sprintf(current, "%d",t -> tm_year + 1900);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mon + 1);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mday);
+	 *(current + len) = ' ';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_hour);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_min);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_sec);
 }
