4e6d90799eef517b0ccd2084a703e9499ea3f40f
3q12 <si8363@naver.com>
2016-11-21 12:58:19 +0900

    ad

--- my_fs.c
+++ my_fs.c
@@ -1,29 +1,24 @@
 #include<stdio.h>
 #include<time.h>
 
-void INPUT_TIME(char *current){
-union type 
-{
-	 block *indirectnode[16];
+void INPUT_TIME(char *current);
+typedef struct Block{
+	 struct Block *indirectnode[16];
 	 char file[128];
-}
-
-typedef struct{
-	 union type;
-}block;
+}Block;
 typedef struct{
 
 }File;
 typedef struct{
-	 
+
 }Dir;
 typedef struct{
 	 _Bool ForD;
 	 char time[20];
 	 int File_size;
-	 block *direct;
-	 block *indirect;
-	 block *double_indirect
+	 Block *direct;
+	 Block *indirect;
+	 Block *double_indirect;
 }Inode;
 int main()
 {
@@ -32,27 +27,27 @@ int main()
 
 
 void INPUT_TIME(char *current){
-	struct tm *t;
-	time_t timer;
-	int len;
-	timer = time(NULL);
-	t = localtime(&timer);
+	 struct tm *t;
+	 time_t timer;
+	 int len;
+	 timer = time(NULL);
+	 t = localtime(&timer);
 
-	len = sprintf(current, "%d",t -> tm_year + 1900);
-	*(current + len) = '/';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_mon + 1);
-	*(current + len) = '/';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_mday);
-	*(current + len) = ' ';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_hour);
-	*(current + len) = ':';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_min);
-	*(current + len) = ':';
-	len++;
-	len += sprintf(current + len, "%d", t -> tm_sec);
+	 len = sprintf(current, "%d",t -> tm_year + 1900);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mon + 1);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mday);
+	 *(current + len) = ' ';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_hour);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_min);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_sec);
 }
 
