8efbb24824961a0f9d97ce6d21b0e0909ae5d610
3q12 <si8363@naver.com>
2016-11-21 12:23:38 +0900

    addtime

--- my_fs.c
+++ my_fs.c
@@ -1,23 +1,58 @@
 #include<stdio.h>
-#include<time.h>a
+#include<time.h>
 
+void INPUT_TIME(char *current){
 union type 
 {
-	 block *indirectnodei[16];
-	 char body[128];
+	 block *indirectnode[16];
+	 char file[128];
 }
 
 typedef struct{
 	 union type;
 }block;
 typedef struct{
+
+}File;
+typedef struct{
 	 
 }Dir;
 typedef struct{
 	 _Bool ForD;
-	// time
+	 char time[20];
 	 int File_size;
 	 block *direct;
 	 block *indirect;
 	 block *double_indirect
 }Inode;
+int main()
+{
+
+}
+
+
+void INPUT_TIME(char *current){
+	struct tm *t;
+	time_t timer;
+	int len;
+	timer = time(NULL);
+	t = localtime(&timer);
+
+	len = sprintf(current, "%d",t -> tm_year + 1900);
+	*(current + len) = '/';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_mon + 1);
+	*(current + len) = '/';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_mday);
+	*(current + len) = ' ';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_hour);
+	*(current + len) = ':';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_min);
+	*(current + len) = ':';
+	len++;
+	len += sprintf(current + len, "%d", t -> tm_sec);
+}
+
