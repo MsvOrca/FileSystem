23f091c8b86774f855db80207c6f264432b425df
3q12 <si8363@naver.com>
2016-11-22 15:43:04 +0900

    shell

--- my_fs.c
+++ my_fs.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 #include<time.h>
+#include<stdlib.h>
+#include<string.h>
+
+char Usrinput[100];
+char Usrcmd[20];
+char Usrbuf1[10];
+char Usrbuf2[10];
+char Usrbuf3[10];
+
 
 void INPUT_TIME(char *current);
+void USER_INPUT();
+int CLASSIFY_INPUT(char *buf,int i);
 void MY_LS();
 void MY_CAT();
 void MY_SHOWFILE();
@@ -48,9 +59,54 @@ typedef struct{
 }Inode;
 int main()
 {
-
+USER_INPUT();
+printf("%s\n%s\n%s\n%s\n",Usrcmd,Usrbuf1,Usrbuf2,Usrbuf3);
+//	 while(1)
+//	 {
+//		  printf("[%s$]",directory);//now dir(pwd)
+//		  scanf("%s",&Usrint);
+	//	  switch(1)
+	//	  {
+	//			case 	
+	//	  }
+//	 }
+	 return 0;
 }
-
+void printTest(char *a)
+{
+	 int i = 0;
+	 printf("PrintTest ====\n");
+	 while(a[i] != 0)
+	 {
+		printf("%d ",a[i]);
+		++i;
+	 }
+	printf("\nPrintfTestEnd =====\n");
+}
+void USER_INPUT()
+{
+	 int i=0;
+	 scanf("%[^\n]",Usrinput);
+	 i=CLASSIFY_INPUT(Usrcmd,i);
+	 i=CLASSIFY_INPUT(Usrbuf1,i);
+	 i=CLASSIFY_INPUT(Usrbuf2,i);
+	 CLASSIFY_INPUT(Usrbuf3,i);
+}
+int CLASSIFY_INPUT(char buf[],int i)
+{
+	 int x=0;
+	 for(;i<100;i++)
+	 {
+		  if(Usrinput[i]==' ' || Usrinput[i]=='\0')
+		  {
+				buf[x]=0;
+				break;
+		  }
+		  buf[x]=Usrinput[i];
+		  x++;
+	 }
+	 return i+1;
+}
 void INPUT_TIME(char *current){
 	 struct tm *t;
 	 time_t timer;
