1e21a0d7c38937b34b1989c5cef2a4a49de186f6
MsvOrca <wjp229@naver.com>
2016-11-25 18:06:19 +0900

    시간 변경

--- time.c
+++ time.c
@@ -12,7 +12,11 @@ typedef struct{//시간에 대한 구조체
 typedef struct{
 	Touched *made;//아이노드에 넣을 구조체 예시
 }Inode;
-void INPUT_TIME(Inode test, struct tm *t, time_t timer){
+void INPUT_TIME(Inode test){
+	struct tm *t;
+	time_t timer;
+	timer = time(NULL);
+	t = localtime(&timer);
 
 	test.made->year = t -> tm_year + 1900;
 	test.made->mon = t -> tm_mon + 1;
@@ -26,13 +30,9 @@ void INPUT_TIME(Inode test, struct tm *t, time_t timer){
 int main()
 {
 	Inode test;
-	struct tm *t;
-	time_t timer;
-	timer = time(NULL);
-	t = localtime(&timer);
 	test.made = malloc(sizeof(Touched));
 
-	INPUT_TIME(test, t, timer);
+	INPUT_TIME(test);
 
 	printf("%d/%d/%d %d:%d:%d", test.made->year, test.made->mon, test.made->day, test.made->hour, test.made->min, test.made->sec);
 	return 0;
