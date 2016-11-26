20bc2dd031cb7498c4b58b631d0acb54f234512f
MsvOrca <wjp229@naver.com>
2016-11-26 13:26:23 +0900

    수정!

---
+++ f2.c
@@ -0,0 +1,41 @@
+#include"func.h"
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+
+void INPUT_TIME(char *current){
+	 struct tm *t;
+	 time_t timer;
+	 int len;
+	 timer = time(NULL);
+	 t = localtime(&timer);
+
+	 len = sprintf(current, "%d",t -> tm_year + 1900);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mon + 1);
+	 *(current + len) = '/';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_mday);
+	 *(current + len) = ' ';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_hour);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_min);
+	 *(current + len) = ':';
+	 len++;
+	 len += sprintf(current + len, "%d", t -> tm_sec);
+}
+
+void MY_PWD()
+{}
+void MY_CD()
+{}
+void MY_MKDIR()
+{}
+void MY_RMDIR()
+{}
+void MY_TREE()
+{}
